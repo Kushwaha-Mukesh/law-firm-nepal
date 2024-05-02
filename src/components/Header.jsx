703f4289cfc402d8ca28b7134ffbb0d07243c8d8
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-05-02 22:38:28 +0545

    added responsive navbar with some changes in color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
+"use client";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <div className="flex justify-around items-center pt-4">
+    <div className="flex justify-around items-center pt-4 relative">
       <div>
         <img
           className="w-20 h-20 rounded-full"
@@ -8,7 +14,7 @@ const Header = () => {
           alt="logo-img"
         />
       </div>
-      <div className="w-1/2 flex justify-between">
+      <div className="w-1/2 md:flex justify-between hidden">
         <span className="hover:bg-[#cc998b] rounded-lg text-[#F5F5DC] px-2 py-1 cursor-pointer">
           Home
         </span>
@@ -22,6 +28,34 @@ const Header = () => {
           Contact Us
         </span>
       </div>
+      {showMenu ? (
+        <RxCross2
+          className="md:hidden text-3xl text-white cursor-pointer"
+          onClick={() => setShowMenu(false)}
+        />
+      ) : (
+        <RxHamburgerMenu
+          className="md:hidden text-3xl text-white cursor-pointer"
+          onClick={() => setShowMenu(true)}
+        />
+      )}
+
+      {showMenu && (
+        <div className="w-[200px] flex flex-col items-start gap-6 py-6 px-2 absolute top-20 right-0 h-screen bg-[#2b2324]">
+          <span className="hover:bg-[#cc998b] rounded-lg text-[#F5F5DC] px-2 py-1 cursor-pointer">
+            Home
+          </span>
+          <span className="hover:bg-[#cc998b] rounded-lg text-[#F5F5DC] px-2 py-1 cursor-pointer">
+            About Us
+          </span>
+          <span className="hover:bg-[#cc998b] rounded-lg text-[#F5F5DC] px-2 py-1 cursor-pointer">
+            Services
+          </span>
+          <span className="hover:bg-[#cc998b] rounded-lg text-[#F5F5DC] px-2 py-1 cursor-pointer">
+            Contact Us
+          </span>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,13 @@
 const HeroSection = () => {
   return (
-    <div className="h-[500px] flex flex-col justify-center gap-4 pl-24 mt-8">
-      <p className="text-sm text-[#F5F5DC]">FOUNDED BY EXPERT LAWYERS</p>
-      <h1 className="text-3xl text-[#F5F5DC]">
+    <div className="h-[500px] flex flex-col justify-center gap-4 pl-8 md:pl-24 mt-8">
+      <p className="text-xs sm:text-sm text-[#F5F5DC]">
+        FOUNDED BY EXPERT LAWYERS
+      </p>
+      <h1 className="text-2xl sm:text-3xl text-[#F5F5DC]">
         Online Notary Service in Nepal
       </h1>
-      <p className="text-xl text-[#F5F5DC]">
+      <p className="text-lg sm:text-xl text-[#F5F5DC]">
         Unlocking Authenticity: Your Trusted Notary and Translation Partner
       </p>
       <button className="self-start text-lg border-[#864c3b] bg-[#864c3b] text-[#F5F5DC] px-4 py-2 rounded-lg">
@@ -16,9 +18,3 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
-
-// White (#FFFFFF)
-// Beige (#F5F5DC)
-// Soft Blue (#ADD8E6)
-// Gold (#FFD700)
-// Ruby Red (#E0115F)
